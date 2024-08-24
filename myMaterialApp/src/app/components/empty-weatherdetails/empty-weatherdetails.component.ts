3249c718d4ba5037b4d4f8433c2b4b5355901398
sigmazen <sigmazen@hotmail.com>
2024-08-24 15:52:23 -0700

    Router code complete

---
+++ myMaterialApp/src/app/components/empty-weatherdetails/empty-weatherdetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-weatherdetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './empty-weatherdetails.component.html',
+  styleUrl: './empty-weatherdetails.component.css'
+})
+export class EmptyWeatherdetailsComponent {
+
+}
--- myMaterialApp/src/app/components/weather/weather-details/weather-details.component.ts
+++ myMaterialApp/src/app/components/weather/weather-details/weather-details.component.ts
@@ -1,17 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Forecast } from '../../../core/interfaces/forecast';
+import { CityService } from '../../../core/services/city.service';
 
 @Component({
   selector: 'app-weather-details',
   standalone: true,
   imports: [],
   templateUrl: './weather-details.component.html',
-  styleUrl: './weather-details.component.scss'
+  styleUrl: './weather-details.component.scss',
 })
-
-
 export class WeatherDetailsComponent {
-  @Input() 
-  forecast: Forecast = { conditions: '', wind: { speed: 0, direction: '' }, temperature: { day: { min: 0, max: 0 }, night: { min: 0, max: 0 } } };
-}
+  forecast: Forecast;
 
+  constructor(private route: ActivatedRoute, private cityService: CityService) {
+    this.forecast = {
+      cityId: -1,
+      conditions: '',
+      wind: { speed: 0, direction: '' },
+      temperature: { day: { min: 0, max: 0 }, night: { min: 0, max: 0 } },
+    };
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) =>
+      this.initializeForecast(+params['id'])
+    );
+  }
+
+  initializeForecast(id: number) {
+    this.forecast = this.cityService.getForecast(id);
+  }
+}
